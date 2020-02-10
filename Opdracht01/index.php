0693b8584aedfa69f8610f5a0216d49c90de5990
Diego Cubero van Coevorden <diego.cuberovancoevorden@edu-kw1c.nl>
2020-02-10 11:43:59 +0100

    Opdracht01

--- Opdracht01/index.php
+++ Opdracht01/index.php
@@ -5,3 +5,37 @@
  * Time: 11:06
  * File: index.php
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title><?php print "pagina 1"; ?></title>
+</head>
+<body>
+    <header>
+        <h1><?php echo "Uitwerking van PHP-Opdrachten"; ?></h1>
+    </header>
+    <aside>
+        <h2>Menu</h2>
+        <ul>
+            <li>Hoofdstuk 2
+                <ul>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+</body>
+</html>
+
+
